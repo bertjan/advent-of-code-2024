f97931f459f5b7ce1e399486c957802cc22f0f98
Bert Jan Schrijver <bertjan@openvalue.nl>
2024-12-03 07:17:31 +0100

    day 3: shorten a bit by utilizing capturing groups

--- src/main/java/AoC2024Day3.java
+++ src/main/java/AoC2024Day3.java
@@ -2,11 +2,10 @@ void main() throws Exception {
     var lines = Files.readAllLines(Path.of("day3/input.txt"));
     long result = 0, result2 = 0; boolean enabled = true;
     for (String line : lines) {
-        Matcher matcher = Pattern.compile("(mul\\(\\d{1,3},\\d{1,3}\\)|don't\\(\\)|do\\(\\))").matcher(line);
+        Matcher matcher = Pattern.compile("mul\\((\\d{1,3}),(\\d{1,3})\\)|(do(n't)*)\\(\\)").matcher(line);
         while (matcher.find()) { switch (matcher.group()) {
             case "do()": enabled = true; break; case "don't()": enabled = false; break;
-            default: var parts = matcher.group().replaceAll("[^\\d,]", "").split(",");
-                     long product = Long.valueOf(parts[0]) * Long.valueOf(parts[1]);
+            default: long product = Long.valueOf(matcher.group(1)) * Long.valueOf(matcher.group(2));
                      result += product; if (enabled) result2 += product;
     }}}
     System.out.println(result + " " + result2);
