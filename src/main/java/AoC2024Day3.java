6b1d48d6591a77e84383365e4882cf830be6ae62
Bert Jan Schrijver <bertjan@openvalue.nl>
2024-12-03 07:29:05 +0100

    day 3: shorten a bit

--- src/main/java/AoC2024Day3.java
+++ src/main/java/AoC2024Day3.java
@@ -1,7 +1,7 @@
 void main() throws Exception {
     var lines = Files.readAllLines(Path.of("day3/input.txt"));
     long result = 0, result2 = 0; boolean enabled = true;
-    for (String line : lines) {
+    for (String line: lines) {
         Matcher matcher = Pattern.compile("mul\\((\\d{1,3}),(\\d{1,3})\\)|(do(n't)*)\\(\\)").matcher(line);
         while (matcher.find()) { switch (matcher.group()) {
             case "do()": enabled = true; break; case "don't()": enabled = false; break;
